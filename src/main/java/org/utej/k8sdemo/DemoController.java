a024bf34e35734bfeeead4b2171a2d5db10cbf0e
Desky <91786744+utej8553@users.noreply.github.com>
2025-11-18 19:58:58 +0530

    Add files via upload

---
+++ src/main/java/org/utej/k8sdemo/DemoController.java
@@ -0,0 +1,18 @@
+package org.utej.k8sdemo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api")
+public class DemoController {
+    @GetMapping("/home")
+    public String home() {
+        return "You just visited /home";
+    }
+    @GetMapping("/end")
+    public String end() {
+        return "You just visited /end";
+    }
+}
